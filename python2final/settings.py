4d9fd675739f7c1f665983a9553d1806d906ca27
Zhernossek Daniyal <201240@astanait.edu.kz>
2022-03-04 00:24:31 +0600

    changes

--- python2final/settings.py
+++ python2final/settings.py
@@ -132,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
